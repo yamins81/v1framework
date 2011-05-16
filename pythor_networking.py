64dafb17c6d61e01c23f4b0b62b69bc5004e3e74
Dan Yamins <dyamins@gmail.com>
2011-05-16 12:40:35 -0400

    network cache using zmq

---
+++ pythor_networking.py
@@ -0,0 +1,54 @@
+import zmq
+
+NETWORK_CACHE_PORT = '5555' 
+
+def network_cache_L(L,port=None):
+    if port is None:
+        port = NETWORK_CACHE_PORT
+    ctx = zmq.Context()
+    sock = ctx.socket(zmq.REP)
+    sock.bind("tcp://127.0.0.1:" + str(port))
+
+    cache = {}
+    L = set(L)
+    done = set([])
+    while True:
+        req = sock.recv_pyobj()
+        if 'get'  in req:
+            if req['get'] in cache:
+                sock.send_pyobj(cache[req['get']])
+            else:
+                sock.send_pyobj(None)
+        elif 'put' in req:
+            cache[req['put'][0]] = req['put'][1]
+            sock.send_pyobj(True)
+        else:
+            sock.send_pyobj(True)
+            done.add(req['DONE'])
+        
+        if done == L:
+            break
+
+def network_cache(port=None):
+    if port is None:
+        port = NETWORK_CACHE_PORT 
+    ctx = zmq.Context()
+    sock = ctx.socket(zmq.REP)
+    sock.bind("tcp://127.0.0.1:" + str(port))
+
+    cache = {}
+    L = set(L)
+    done = set([])
+    while True:
+        req = sock.recv_pyobj()
+        if 'get'  in req:
+            if req['get'] in cache:
+                sock.send_pyobj(cache[req['get']])
+            else:
+                sock.send_pyobj(None)
+        elif 'put' in req:
+            cache[req['put'][0]] = req['put'][1]
+            sock.send_pyobj(True)
+        else:
+            sock.send_pyobj(True)
+
