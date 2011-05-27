dfeda0e5ad8aa0b714e51d08413b8a9358ab5363
Dan Yamins <dyamins@gmail.com>
2011-05-27 15:04:15 -0400

    3d model categories

---
+++ 3d_model_categories.py
@@ -0,0 +1,55 @@
+
+MODEL_CATEGORIES = {'cars' : ['MB26897','MB28855','MB27827',
+        'MB28498','MB28343','MB29782','MB31083',
+        'MB31079','MB31620','MB28490','MB31095',
+        'MB28490','MB27803','MB31518','MB29642'],
+ 
+'cats_and_dogs' : ['cheetah','lion','semilonghair_cat','shorthair_cat',
+                 'leopard','lynx','oriental','panther',
+                 'golden_retriever','doberman','dalmatian','schnauzer',
+                 'wiemaraner','bloodhound','bullmastiff','chihuahua'],
+
+'reptiles' : ['MB30418','MB31192','MB29694',
+           'adder','boa','bullfrog','chameleon',
+           'crocodile','gecko','iguana','leatherback',
+           'terapin','tortoise','treefrog','salamander'],
+           
+
+'guns' : ['MB27069','MB27350','MB30684',
+        'MB27860','MB27346','MB29066','MB29726',
+        'MB30472','MB30027','MB30680'],
+
+'boats' : ['MB27840','MB27239','MB28586',
+         'MB28586','MB29022','MB28646','MB28065',
+         'MB29366','MB29346','MB29698','MB29654',
+         'MB29762','MB31331','MB29937','MB30676'],
+
+'planes' : ['MB26937','MB27203','MB27211','MB27309',
+          'MB27463','MB27876','MB27459','MB27732',
+          'MB27530','MB28355','MB28430','MB29650',
+          'MB30047','MB29894','MB29798','MB28651'],
+
+'faces' : ['face0001','face0002','face0003','face0004',
+         'face0005','face0006','face0007','face0008',
+         'face1','face2','face3','face4',
+         'face5','face6','face7','face8'],
+
+'chair' : ['MB29826','MB29342','MB28514','MB27139',
+         'MB27680','MB27675','MB27692','MB27688',
+         'MB27684','MB27679','MB27696','MB27708',
+         'MB28045','MB27704','MB27700'],
+
+'table' : ['MB30926','MB30374','MB30082','MB28811',
+         'MB28462','MB28077','MB28049','MB30386',
+         'MB30926','MB28214','MB27386','MB28137'],
+
+'plants' : ['MB29862','MB30366','MB28415','MB30422',
+        'MB30150','MB31632','MB30370','MB30114',
+        'MB30762','MB30814','MB29179','MB27028',
+        'MB26957','MB27251','MB28291','MB27736'],
+
+'buildings' : ['MB27471','MB28886','MB28807','MB27191',
+             'MB29870','MB31200','MB27835','MB31048',
+             'MB27254','MB31040','MB31131','MB29802',
+             'MB30810','MB28921','MB28934','MB30794']}
+
