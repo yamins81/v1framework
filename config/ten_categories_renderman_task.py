7e2cb35b53f2b632c45c25f0b7008ccf2daacb4b
Dan Yamins <dyamins@gmail.com>
2011-06-13 17:32:25 -0400

    added ten_categories task config

---
+++ config/ten_categories_renderman_task.py
@@ -0,0 +1,26 @@
+from bson import SON
+
+import model_categories as mc
+
+config = {
+
+
+'train_test' : [
+
+   SON([
+      ('transform_average', SON([('transform_name','sum_up')])),
+      ('N',1), 
+      ('ntrain',240),
+      ('ntest',42),
+      ('universe',SON([('image.tx',SON([('$lt',.22),('$gt',-.22)])),
+                       ('image.ty',SON([('$lt',.22),('$gt',-.22)]))])),
+      ('query',[SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['cars'])]))]),
+                SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['planes'])]))])
+               ])
+      ])
+   ]
+   
+   
+}
+
+
