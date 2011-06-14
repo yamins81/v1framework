408c5584693c2f71a3a5ec2e7546433947f97fba
Dan Yamins <dyamins@gmail.com>
2011-06-14 16:11:44 -0400

    gray backgrounds

--- config/ten_categories_renderman_task.py
+++ config/ten_categories_renderman_task.py
@@ -38,6 +38,17 @@ config = {
       ('query',[SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['cars'])]))]),
                 SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['planes'])]))])
                ])
+      ]),
+   SON([
+      ('N',1), 
+      ('ntrain',100),
+      ('ntest',30),
+      ('universe',SON([('image.ty',SON([('$lt',.5),('$gt',-.5)])),
+                       ('image.tz',SON([('$lt',.5),('$gt',-.5)])),
+                       ('image.bg_id','gray.tdl')])),
+      ('query',[SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['cars'])]))]),
+                SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['planes'])]))])
+               ])
       ])
    ]
    
