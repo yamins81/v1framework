9491fa9315137f4907ee2339f4bd51bf5823fc15
Dan Yamins <dyamins@gmail.com>
2011-06-15 10:38:53 -0400

    more tasks

--- config/various_vs_polygon_task0.py
+++ config/various_vs_polygon_task0.py
@@ -14,6 +14,15 @@ config = {
                        ('$where' , '(this.config.image.sx / this.config.image.sy < 2.0) && (this.config.image.sx / this.config.image.sy > 1/2.0)')])),
       ('query',[SON([('image.object',SON([('$size',5)]))]),SON([('image.object',SON([('$size',7)]))]),SON([('image.object.type','rectangle')]),SON([('image.object.type','arc')])])
    ]),
+  SON([
+      ('N',1), 
+      ('ntrain',160),
+      ('ntest',40),
+      ('universe',SON([('image.tx',SON([('$lt',.22),('$gt',-.22)])),
+                       ('image.ty',SON([('$lt',.22),('$gt',-.22)])),
+                       ('$where' , '(this.config.image.sx / this.config.image.sy < 2.0) && (this.config.image.sx / this.config.image.sy > 1/2.0)')])),
+      ('query',SON([('image.object.type','rectangle')]))
+   ]),
    ]
    
    
