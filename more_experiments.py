b55cbdd256456e2e851af616b676c40f48a5806a
Dan Yamins <dyamins@gmail.com>
2011-06-19 13:54:47 -0400

    medium-throughput experiment random

--- more_experiments.py
+++ more_experiments.py
@@ -12,3 +12,18 @@ def ext_eval_best_l3_exploratory_renderman(depends_on=('../config/exploratory_re
                                             '../config/ten_categories_images.py',
                                             convolve_func_name='numpy', write=True,parallel=False)
                                             
+
+
+@protocolize()
+def make_various_random_l3_models(depends_on='../config/various_random_l3_models.py'):
+    protocols.model_protocol(depends_on,parallel=False,write=True)
+
+@protocolize()
+def ext_eval_various_random_l3_various_renderman(depends_on=('../config/parallel_renderman_tasks_for_random_l3.py',
+                                                  '../config/various_random_l3_models.py',
+                                                  '../config/ten_categories_images.py')):
+    protocols.extract_and_evaluate_protocol('../config/parallel_renderman_tasks_for_random_l3.py',
+                                            '../config/various_random_l3_models.py',
+                                            '../config/ten_categories_images.py',
+                                            convolve_func_name='numpy', write=True,parallel=True)
+                                            
