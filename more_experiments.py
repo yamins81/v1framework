fc0aac18357559eb1c945df96ce96c5f55f5faf8
Dan Yamins <dyamins@gmail.com>
2011-06-20 11:18:54 -0400

    medium-scale polygon evaluation

--- more_experiments.py
+++ more_experiments.py
@@ -29,13 +29,33 @@ def ext_eval_various_random_l3_various_renderman(depends_on=('../config/parallel
                                             
 @protocolize()
 def make_best_l3_grayscale_model(depends_on='../config/best_l3_grayscale_model.py'):
+    """
+    best l3 model but converts images to grayscale in preprocessing
+    """
     protocols.model_protocol(depends_on,parallel=False,write=True)
 
 @protocolize()
 def ext_eval_best_l3_exploratory_grayscale_renderman(depends_on=('../config/exploratory_renderman_tasks2.py',
                                                   '../config/best_l3_grayscale_model.py',
                                                   '../config/ten_categories_images.py')):
+    """
+    testing grayscale best l3 on two hard renderman problems.  result:   about 29% for each problem
+    """
     protocols.extract_and_evaluate_protocol('../config/exploratory_renderman_tasks2.py',
                                             '../config/best_l3_grayscale_model.py',
                                             '../config/ten_categories_images.py',
                                             convolve_func_name='numpy', write=True,parallel=False)
+
+
+@protocolize()
+def make_various_random_l3_grayscale_models(depends_on='../config/various_random_l3_grayscale_models.py'):
+    protocols.model_protocol(depends_on,parallel=False,write=True)
+
+@protocolize()
+def ext_eval_various_random_l3_grayscale_various_polygon(depends_on=('../config/parallel_polygon_tasks_for_random_l3.py',
+                                                  '../config/various_random_l3_grayscale_models.py',
+                                                  '../config/polygon_task.py')):
+    protocols.extract_and_evaluate_protocol('../config/parallel_polygon_tasks_for_random_l3.py',
+                                            '../config/various_random_l3_grayscale_models.py',
+                                            '../config/polygon_task.py',
+                                            convolve_func_name='numpy', write=True,parallel=True)
