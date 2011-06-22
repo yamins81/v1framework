40d7d3b947411ba947bc54128fa583e66eccd8d5
Dan Yamins <dyamins@gmail.com>
2011-06-21 20:27:08 -0400

    same conv

--- config/cars_vs_planes_task_for_freq.py
+++ config/cars_vs_planes_task_for_freq.py
@@ -18,6 +18,16 @@ config = {
                ])
       ]),      
    SON([
+      ('transform_average', SON([('transform_name','translation')])),
+      ('N',2), 
+      ('ntrain',80),
+      ('ntest',40),
+      ('universe',SON([('image.bg_id',SON([('$ne','gray.tdl')]))])),
+      ('query',[SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['cars'])]))]),
+                SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['planes'])]))])
+               ])
+      ]),      
+   SON([
       ('N',1), 
       ('ntrain',80),
       ('ntest',40),
