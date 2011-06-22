76e6779287751b564ec3ab268d98e1a3fa87a90a
Dan Yamins <dyamins@gmail.com>
2011-06-21 20:05:51 -0400

    same conv

--- config/cars_vs_planes_task_for_freq.py
+++ config/cars_vs_planes_task_for_freq.py
@@ -17,7 +17,15 @@ config = {
                 SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['planes'])]))])
                ])
       ]),      
-   
+   SON([
+      ('N',1), 
+      ('ntrain',80),
+      ('ntest',40),
+      ('universe',SON([('image.bg_id',SON([('$ne','gray.tdl')]))])),
+      ('query',[SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['cars'])]))]),
+                SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['planes'])]))])
+               ])
+      ]),      
    ]
    
    
