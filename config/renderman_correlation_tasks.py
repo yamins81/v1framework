cd1a0dad5e2acc96ffd1eccd9839c99cc68f4769
Daniel Yamins <danielyamins@Daniel-Yaminss-Mac-Pro.local>
2011-06-25 17:33:57 -0400

    more correlation stuff

--- config/renderman_correlation_tasks.py
+++ config/renderman_correlation_tasks.py
@@ -1,9 +1,17 @@
 from bson import SON
 
+import model_categories as mc
+
 config = {
 
 'extractions' : [
       SON([('sample_size',2000),
+           ('ker_shape',[5,5])])
+      SON([('sample_size',2000),
+           ('query',SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['faces'])]))])),
+           ('ker_shape',[5,5])])
+      SON([('sample_size',2000),
+           ('query',SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['cars']+mc.MODEL_CATEGORIES['planes'])]))])),
            ('ker_shape',[5,5])])
    ]
    
