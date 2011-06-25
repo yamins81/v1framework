65909414d55c5ae98c41159101114fe071d8cf39
Daniel Yamins <danielyamins@Daniel-Yaminss-Mac-Pro.local>
2011-06-25 15:43:39 -0400

    more correlation stuff

---
+++ config/renderman_correlation_tasks.py
@@ -0,0 +1,11 @@
+from bson import SON
+
+config = {
+
+'extractions' : [
+      SON([('sample_size',2000),
+           ('ker_shape',[5,5])])
+   ]
+   
+   
+}
--- correlation_experiments.py
+++ correlation_experiments.py
@@ -19,3 +19,13 @@ def correlation_l1_gabor_polygon(depends_on=('../config/polygon_correlation_task
                                 '../config/various_l1_gabors.py',
                                 '../config/polygon_task.py',
                                 convolve_func_name='numpy', write=True,parallel=True)
+
+
+@protocolize()
+def correlation_l1_gabor_renderman(depends_on=('../config/renderman_correlation_tasks.py',
+                                                  '../config/various_l1_gabors.py',
+                                                  '../config/ten_categories_images.py')):
+    protocols.get_corr_protocol('../config/renderman_correlation_tasks.py',
+                                '../config/various_l1_gabors.py',
+                                '../config/ten_categories_images.py',
+                                convolve_func_name='numpy', write=True,parallel=True)
