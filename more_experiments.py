21dbee5d88ca0bb69be36fc828d746386652f946
Daniel Yamins <danielyamins@Daniel-Yaminss-Mac-Pro.local>
2011-06-26 10:26:03 -0400

    correct comparison for correlation experiments

--- more_experiments.py
+++ more_experiments.py
@@ -352,6 +352,13 @@ def make_various_l2_gabor_random_models(depends_on='../config/various_l2_gabor_r
     protocols.model_protocol(depends_on,parallel=False,write=True)
     
 @protocolize()
+def make_various_l2_freq_from_l1_gabor_models(depends_on='../config/various_l2_freq_from_l1_gabor_models.py'):
+    """
+  
+    """
+    protocols.model_protocol(depends_on,parallel=False,write=True)
+    
+@protocolize()
 def ext_eval_various_l2_freq_exploratory_renderman(depends_on=('../config/exploratory_renderman_tasks_for_freq.py',
                                                   '../config/various_l2_freq_models.py',
                                                   '../config/ten_categories_images.py')):
@@ -533,6 +540,20 @@ def ext_eval_various_l2_gabor_random_various_polygon(depends_on=('../config/para
                                             '../config/polygon_task.py',
                                             convolve_func_name='numpy', write=True,parallel=True)
                                             
+
+@protocolize()
+def ext_eval_various_l2_freq_from_l1_gabor_various_polygon(depends_on=('../config/parallel_polygon_tasks_for_freq.py',
+                                                  '../config/various_l2_freq_from_l1_gabor_models.py',
+                                                  '../config/polygon_task.py')):
+    """
+
+    
+    """
+    protocols.extract_and_evaluate_protocol('../config/parallel_polygon_tasks_for_freq.py',
+                                            '../config/various_l2_freq_from_l1_gabor_models.py',
+                                            '../config/polygon_task.py',
+                                            convolve_func_name='numpy', write=True,parallel=True)
+
                                             
 
 @protocolize()
