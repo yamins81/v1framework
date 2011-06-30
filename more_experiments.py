e626badfb3d2db3d7aa0bb9b850b3bd6cec60719
Dan Yamins <dyamins@gmail.com>
2011-06-30 14:38:42 -0400

    more testing

--- more_experiments.py
+++ more_experiments.py
@@ -1153,10 +1153,31 @@ def ext_eval_l1_gabor_test_2_polygon(depends_on=('../config/parallel_polygon_tas
                                                   '../config/l1_gabor_test_model_2.py',
                                                   '../config/polygon_task.py')):
     """
-    
+    seeing if adding more filters to the best l1 gabor helps -- yes a little, 87.8% (try more?)
     
     """
     protocols.extract_and_evaluate_protocol('../config/parallel_polygon_tasks_for_ht.py',
                                             '../config/l1_gabor_test_model_2.py',
                                             '../config/polygon_task.py',
                                             convolve_func_name='numpy', write=True,parallel=True)                                                                                                             
+                                            
+@protocolize()
+def make_l1_gabor_test_model_3(depends_on='../config/l1_gabor_test_model_3.py'):
+    """
+
+    """
+    protocols.model_protocol(depends_on,parallel=False,write=True)
+    
+
+@protocolize()
+def ext_eval_l1_gabor_test_3_polygon(depends_on=('../config/parallel_polygon_tasks_for_ht.py',
+                                                  '../config/l1_gabor_test_model_3.py',
+                                                  '../config/polygon_task.py')):
+    """
+    seeing if adding even more filters helps ... 
+    
+    """
+    protocols.extract_and_evaluate_protocol('../config/parallel_polygon_tasks_for_ht.py',
+                                            '../config/l1_gabor_test_model_3.py',
+                                            '../config/polygon_task.py',
+                                            convolve_func_name='numpy', write=True,parallel=True)                                                                                                                                                         
