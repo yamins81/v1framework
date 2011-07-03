4bc79d83c8455457563961ee2f7200ed5303d803
Daniel Yamins <dyamins@gmail.com>
2011-07-03 01:15:24 -0400

    ht gabor3d

--- more_experiments.py
+++ more_experiments.py
@@ -1229,3 +1229,23 @@ def ext_eval_various_l2_gabor_gabor_renderman(depends_on=('../config/renderman_t
                                             '../config/ten_categories_images.py',
                                             convolve_func_name='numpy', write=True,parallel=True)
                                             
+                                            
+@protocolize()
+def make_ht_l2_gabor_gabor_models(depends_on='../config/ht_l2_gabor_gabor_models.py'):
+    """
+
+    """
+    protocols.model_protocol(depends_on,parallel=False,write=True) 
+ 
+@protocolize()
+def ext_eval_ht_l2_gabor_gabor_renderman(depends_on=('../config/renderman_tasks_for_ht.py',
+                                                  '../config/ht_l2_gabor_gabor_models.py',
+                                                  '../config/ten_categories_images.py')):
+    """
+
+    """
+    protocols.extract_and_evaluate_protocol('../config/renderman_tasks_for_ht.py',
+                                            '../config/ht_l2_gabor_gabor_models.py',
+                                            '../config/ten_categories_images.py',
+                                            convolve_func_name='numpy', write=True,parallel=True)
+                                            
