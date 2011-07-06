95557a78f0eaa22ab239cd32683bdd82f38195ce
Daniel Yamins <dyamins@gmail.com>
2011-07-05 20:28:43 -0400

    angle-varioued subtasks

--- config/renderman_subtasks_varied.py
+++ config/renderman_subtasks_varied.py
@@ -15,14 +15,19 @@ def combine_ors(ors):
     return map(combine_dicts,itertools.product(*ors))
       
 
-NSEG = 4
+NSEG = 5
       
 queries = [SON([('$or',combine_ors([[SON([('image.model_id',o),
                                           ('image.ryz',SON([('$gt',2*math.pi*(ind % NSEG)/NSEG),
                                                             ('$lt',2*math.pi*(ind % NSEG + 1)/NSEG)]))]) 
                        for (ind,o) in enumerate(mc.MODEL_CATEGORIES[cat])],
                       [SON([('image.s',SON([('$exists',False)]))]),
-                       SON([('image.s',SON([('$lt',1.25),('$gt',1/1.25)]))])]])),
+                       SON([('image.s',SON([('$lt',1.25),('$gt',1/1.25)]))])],
+                      [SON([('image.rxy',SON([('$exists',False)]))]),
+                       SON([('image.rxy',SON([('$lt',.2)]))])], 
+                      [SON([('image.rxz',SON([('$exists',False)]))]),
+                       SON([('image.rxz',SON([('$lt',.2)]))])]
+                    ])),
           ('image.rxy',SON([('$exists',False)])),
           ('image.rxz',SON([('$exists',False)]))]) for cat in ['cars',
                                                       'planes',
@@ -34,6 +39,34 @@ queries = [SON([('$or',combine_ors([[SON([('image.model_id',o),
                                                       'reptiles',
                                                       'cats_and_dogs']
 ]                                
+
+
+control_task = SON([
+      ('transform_average', SON([('transform_name','translation'),('max',True)])),
+      ('N',3), 
+      ('ntrain',270),
+      ('ntest',180),
+      ('universe',SON([('image.bg_id','gray.tdl'),
+                       ('$or',combine_ors([[SON([('image.ryz',SON([('$exists',False)]))]),
+                                            SON([('image.ryz',SON([('$lt',2*math.pi/NSEG)]))])],
+                                           [SON([('image.s',SON([('$exists',False)]))]),
+                                            SON([('image.s',SON([('$lt',1.25),('$gt',1/1.25)]))])],
+                                           [SON([('image.rxy',SON([('$exists',False)]))]),
+                                            SON([('image.rxy',SON([('$lt',.2)]))])], 
+                                           [SON([('image.rxz',SON([('$exists',False)]))]),
+                                            SON([('image.rxz',SON([('$lt',.2)]))])]
+                                           ]))])),
+      ('query',[SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['cars'])]))]),
+                SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['planes'])]))]),
+                SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['boats'])]))]),
+                SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['faces'])]))]),
+                SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['chair'])]))]),
+                SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['table'])]))]),
+                SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['reptiles'])]))]),
+                SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['plants'])]))]),
+                SON([('image.model_id',SON([('$in',mc.MODEL_CATEGORIES['cats_and_dogs'])]))])
+               ])
+      ])    
  
 
 
@@ -49,7 +82,7 @@ task = SON([
 
 
 config = {
-'train_test' : [task]
+'train_test' : [task,control_task]
 }
 
 
