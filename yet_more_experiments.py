fe2fb98babfc405e957ba5bcaaf8150f66cc9f8a
Daniel Yamins <dyamins@gmail.com>
2011-07-11 07:46:11 -0400

    comments

--- yet_more_experiments.py
+++ yet_more_experiments.py
@@ -704,7 +704,7 @@ def ext_eval_ht_l2_gabor_random_variousactivationranges_renderman(depends_on=('.
     """
     47.47, 41.33, 43.75, 44.73, 1.60
     it looks like having ranges helps in the top, and that in particular it helps with L1 activation 
-    around 0, with a range. 
+    around 0, with a nontrivial range
     """
     protocols.extract_and_evaluate_protocol('../config/renderman_tasks_for_ht3.py',
                                             '../config/ht_l2_gabor_random_variousactivationranges_models.py',
