ca3d0405c4e4c017a8894ac2da859eceaf62b88a
Daniel Yamins <dyamins@gmail.com>
2011-07-19 16:16:41 -0400

    rotation task done right

--- even_more_experiments.py
+++ even_more_experiments.py
@@ -579,6 +579,7 @@ def ext_eval_ht_l1_random_top5_polygon_mixedup_renderman(depends_on=('../config/
 def generate_rotated_images():
     Apply()
     
+    
 @Applies(deploy.images,args=('../config/ten_categories_unrotated_images.py',True))
 def generate_unrotated_images():
     Apply()
