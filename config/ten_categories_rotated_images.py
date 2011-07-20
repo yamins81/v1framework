db56d4bdb46adfaaecdbf933a1b8235942efaf3a
Daniel Yamins <dyamins@gmail.com>
2011-07-20 08:42:24 -0400

    remove test

--- config/ten_categories_rotated_images.py
+++ config/ten_categories_rotated_images.py
@@ -21,7 +21,7 @@ base_images = [SON([('model_ids',[m]),
                     ('tz',SON([('$gt',-.6),('$lt',.6)])),
                     ('ryz',SON([('$gt',2*pi*(ind % NSEG)/NSEG),('$lt',2*pi*((ind % NSEG) + 1)/NSEG)]))
                     ])
-               #for (ind,m) in enumerate(MODELS)]
+               for (ind,m) in enumerate(MODELS)]
 
 import copy
 
