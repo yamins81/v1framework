ba5b6ca2fac1d3ee33095995dcedbc9f2fe38842
Daniel Yamins <dyamins@gmail.com>
2011-07-23 15:55:23 -0400

    more levels

--- even_more_experiments.py
+++ even_more_experiments.py
@@ -721,3 +721,18 @@ def ext_eval_fourlevel_test_model_4_renderman(depends_on=('../config/renderman_t
     """
     a,b,c = depends_on
     protocols.extract_and_evaluate_protocol(a,b,c,convolve_func_name='numpy', write=True, parallel='semi')  
+
+@protocolize()
+def make_fourlevel_test_model_5(depends_on='../config/fourlevel_test_model_5.py'):
+    """
+    """
+    protocols.model_protocol(depends_on,parallel=False,write=True)    
+
+@protocolize()
+def ext_eval_fourlevel_test_model_5_renderman(depends_on=('../config/renderman_tasks_for_ht_overlap.py',
+                                                  '../config/fourlevel_test_model_5.py',
+                                                  '../config/ten_categories_images.py')):
+    """
+    """
+    a,b,c = depends_on
+    protocols.extract_and_evaluate_protocol(a,b,c,convolve_func_name='numpy', write=True, parallel='semi')  
