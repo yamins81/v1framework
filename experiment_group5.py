3ce4339ac0789a1a0bf74fa6977e978e2ab51cad
Daniel Yamins <dyamins@gmail.com>
2011-07-27 13:54:08 -0400

    bugfix reptile fourier

--- experiment_group5.py
+++ experiment_group5.py
@@ -16,3 +16,15 @@ def ext_eval_ht_l1_gabor_reptile_vs_plant(depends_on=('../config/reptile_tasks.p
     a,b,c = depends_on
     protocols.extract_and_evaluate_protocol(a,b,c,convolve_func_name='numpy', write=True,parallel='semi')
 
+
+
+@protocolize()
+def ext_eval_ht_l1_gabor_reptile_vs_plant2(depends_on=('../config/reptile_tasks2.py',
+                                                  '../config/ht_l1_gabor_top5_renderman_models.py',
+                                                  '../config/ten_categories_images.py')):
+    """
+            
+    """
+    a,b,c = depends_on
+    protocols.extract_and_evaluate_protocol(a,b,c,convolve_func_name='numpy', write=True,parallel='semi')
+
