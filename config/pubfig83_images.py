f7f885a3405ec57801228ca7acc6349022a9bc64
Dan Yamins <dyamins@gmail.com>
2011-07-30 11:17:20 -0400

    config file for using dataset api

---
+++ config/pubfig83_images.py
@@ -0,0 +1,8 @@
+from bson import SON
+
+pubfig83 = SON([('selection','dataset_api'),
+                 ('generator','dataset_api'),
+                 ('run_with','pythor3.dataset.pubfig83.pubfig83_from_son'),
+                 ('name','pubfig83')])
+
+config = {'images' : [pubfig83]}
