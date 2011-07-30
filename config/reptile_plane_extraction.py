49baf4412785951fd96bc79b95f2cebd8ff1f011
Daniel Yamins <dyamins@gmail.com>
2011-07-30 16:09:06 -0400

    more better protocol stuff

--- config/reptile_plane_extraction.py
+++ config/reptile_plane_extraction.py
@@ -4,9 +4,15 @@ from bson import SON
 
 from model_categories import MODEL_CATEGORIES as CAT
 
+            
 
 extraction =  SON([ ('transform_average',SON([('transform_name','translation'),('max',True)])),
-                    ('query',SON([('image.model_id',SON([('$in',CAT['reptiles'] + CAT['planes'])]))]))
+                    ('query',SON([('image.model_id',SON([('$in',CAT['reptiles'] + CAT['planes'])])),
+                                  ('image.tx',SON([('$exists',False)])),
+                                  ('image.s',SON([('$exists',False)])),
+                                  ('image.rxy',SON([('$exists',False)])),
+                                  ('image.rxz',SON([('$exists',False)])),
+                                 ]))
                   ]) 
                 
     
--- experiment_group5.py
+++ experiment_group5.py
@@ -39,3 +39,24 @@ def ext_eval_various_l2_gabor_gabor_reptile_vs_plant(depends_on=('../config/rept
     a,b,c = depends_on
     protocols.extract_and_evaluate_protocol(a,b,c,convolve_func_name='numpy', write=True,parallel='semi')
 
+
+
+#################extraction for average vectors
+
+
+@protocolize()
+def make_various_l1_gabor_models(depends_on='../config/various_l1_gabor_models.py'):
+    """
+    """
+    protocols.model_protocol(depends_on,parallel=False,write=True)    
+
+
+@protocolize()
+def extract_various_l1_gabors_reptile_and_planes(depends_on=('../config/reptile_plane_extraction.py',
+                                                  '../config/various_l1_gabor_models.py',
+                                                  '../config/ten_categories_images.py')):
+    """
+            
+    """
+    a,b,c = depends_on
+    protocols.extraction_protocol(a,b,c,convolve_func_name='numpy', write=True,parallel=True,save_to_db=True)
