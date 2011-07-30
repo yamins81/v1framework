6071e67981b12fd92f4cb5309499b219e1fe0a2b
Dicarlo-Cox <dicarlocox@gmail.com>
2011-07-30 16:10:30 -0400

    various

--- even_more_experiments.py
+++ even_more_experiments.py
@@ -655,7 +655,7 @@ def ext_eval_ht_l2_gabor_random_o2_top5_pairwise_renderman(depends_on=('../confi
     protocols.extract_and_evaluate_protocol(a,b,c,convolve_func_name='numpy', write=True, parallel='semi')
 
 @protocolize()
-def ext_eval_ht_l2_gabor_random_o2_top5_pairwise_renderman_backgrounds(depends_on=('../config/renderman_pairwise_tasks_backgrounds.py',
+def ext_eval_ht_l2_gabor_random_o2_top5_pairwise_renderman_backgrounds(depends_on=('../config/renderman_pairwise_tasks_background.py',
                                                   '../config/ht_l2_gabor_random_o2_top5_renderman_models.py',
                                                   '../config/ten_categories_images.py')):
     """
