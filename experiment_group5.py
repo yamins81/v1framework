ce244f604d4a7d5fa0821d1334f86d057d091be5
Daniel Yamins <dyamins@gmail.com>
2011-08-01 16:59:57 -0400

    corresponding evaluation

--- experiment_group5.py
+++ experiment_group5.py
@@ -124,6 +124,17 @@ def evaluate_various_l1_gabors_reptile_and_planes(depends_on=('../config/reptile
     a,b,c,d = depends_on
     protocols.evaluate_protocol(a,b,c,d,convolve_func_name='numpy', write=True,parallel=False)
 
+@protocolize()
+def evaluate_various_l1_gabors2_reptile_and_planes(depends_on=('../config/reptile_tasks3.py',
+                                                  '../config/reptile_plane_extraction.py', 
+                                                  '../config/various_l1_gabor_models2.py',
+                                                  '../config/ten_categories_images.py')):
+    """
+            
+    """
+    a,b,c,d = depends_on
+    protocols.evaluate_protocol(a,b,c,d,convolve_func_name='numpy', write=True,parallel=False)
+
 
 @protocolize()
 def evaluate_LRL_various_l1_gabors_reptile_and_planes(depends_on=('../config/reptile_tasks4.py',
