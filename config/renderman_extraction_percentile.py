ef2e5ac994b05a9d0588c62adc9e9a15e8c76678
Dan Yamins <dyamins@gmail.com>
2011-08-24 11:48:50 +0800

    various

--- config/renderman_extraction_percentile.py
+++ config/renderman_extraction_percentile.py
@@ -3,15 +3,9 @@ from starflow.utils import ListUnion
 import copy 
 
 from bson import SON
-
-from model_categories import MODEL_CATEGORIES as CAT
-
-all_models = ListUnion(CAT.values())
             
-
 extraction =  SON([ ('transform_average',SON([('transform_name','translation'),('percentile',[73,80,90,100])])),
-                    ('query',SON([('image.model_id',SON([('$in',all_models)])),
-                                  ('image.bg_id','gray.tdl')
+                    ('query',SON([('image.bg_id','gray.tdl')
                                  ]))
                   ]) 
                 
